5c7a15ae1e85bbb66a0f97b58898d50cfd896224
Kevin Escally <escallyk@gmail.com>
2026-06-18 09:12:22 -0500

    fix: seguridad

--- fiis/src/test/java/com/sgi/fiis/lineas_investigacion/presentation/controller/LineaInvestigacionControllerTest.java
+++ fiis/src/test/java/com/sgi/fiis/lineas_investigacion/presentation/controller/LineaInvestigacionControllerTest.java
@@ -11,6 +11,7 @@ import com.sgi.fiis.shared.domain.exception.ResourceNotFoundException;
 import com.sgi.fiis.shared.infrastructure.exception.GlobalExceptionHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(LineaInvestigacionController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @Import(GlobalExceptionHandler.class)
 class LineaInvestigacionControllerTest {
 
